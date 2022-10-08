f735d3b5d85338ee24a8306158a0aaf13a7c3035
Ranilson de Andrade Saraiva Júnior <ranilsonjunior2@gmail.com>
2022-10-08 12:06:28 -0300

    testes finalizados

--- src/templates/Home/home.spec.jsx
+++ src/templates/Home/home.spec.jsx
@@ -1,11 +1,112 @@
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { Home } from './';
+
+const handlers = [
+  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title 1',
+          body: 'body 1',
+          url: 'img1.jpg',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title 2',
+          body: 'body 2',
+          url: 'img2.jpg',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title 3',
+          body: 'body 3',
+          url: 'img3.jpg',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-  it('is a dummy test', () => {
-    expect(1).toBe(1);
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should render search, posts and load more', async () => {
+    render(<Home />);
+    const noMore = screen.getByText('Não exist Posts!');
+
+    expect.assertions(3);
+
+    await waitForElementToBeRemoved(noMore);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i });
+    expect(images).toHaveLength(2);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should search for posts', async () => {
+    render(<Home />);
+    const noMore = screen.getByText('Não exist Posts!');
+
+    expect.assertions(10);
+
+    await waitForElementToBeRemoved(noMore);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(screen.getByRole('heading', { name: 'title 1 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title 2 2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title 3 3' })).not.toBeInTheDocument();
+
+    userEvent.type(search, 'title1');
+    expect(screen.getByRole('heading', { name: 'Search Value:title1' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title 2 2' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title 3 3' })).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Search Value:title1' })).toBeInTheDocument();
+
+    userEvent.clear(search);
+    expect(screen.getByRole('heading', { name: 'title 1 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title 2 2' })).toBeInTheDocument();
+
+    userEvent.type(search, 'post does not exist');
+    expect(screen.getByText('Não exist Posts!')).toBeInTheDocument();
+  });
+
+  it('should load more posts', async () => {
+    render(<Home />);
+    const noMore = screen.getByText('Não exist Posts!');
+
+    // expect.assertions(3);
+
+    await waitForElementToBeRemoved(noMore);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+
+    userEvent.click(button);
+    expect(screen.getByRole('heading', { name: 'title 3 3' })).toBeInTheDocument();
+    expect(button).toBeDisabled();
   });
 });
 
-// test("renders learn react link", () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+// await waitForElementToBeRemoved(noMorePosts); -> Fala par esperar sumir, e como é assíncrono temos que colocar await, ea função tem que ser async
